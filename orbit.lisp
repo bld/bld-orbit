8d8ecb0462042e002aff6b705a5827d3bce796a6
Ben Diedrich <bldiedrich@gmail.com>
2013-11-12 18:21:53 -0800

    WIP: KS EOM. Fixed some COE <-> RV conversions.

--- orbit.lisp
+++ orbit.lisp
@@ -13,6 +13,11 @@ Includes:
 
 (in-package :bld-orbit)
 
+(defun new-frame (rotor frame)
+  "New reference frame from rotor & original frame"
+  (mapcar #'(lambda (basisvector) (rotateg basisvector rotor)) frame))
+
+
 ;; Define infinity norm method for BLD-ODE Runge-Kutta method on geometric algebra objects
 (defmethod norminfx ((x g))
   (norminf x))
@@ -98,7 +103,8 @@ Includes:
    (t0 :initarg :t0 :initform 0d0 :documentation "Initial time")
    (tf :initarg :tf :initform (* 2 pi) :documentation "Final time")
    (x0 :initarg :x0 :initform (make-instance 'cartstate :r (ve2 :e1 1) :v (ve2 :e2 1)))
-   (rs :initarg :rs :initform (re2 :s 1d0) :documentation "Sail orientation rotor wrt orbital position frame"))
+   (rs :initarg :rs :initform (re2 :s 1d0) :documentation "Sail orientation rotor wrt orbital position frame")
+   (e0 :initarg :e0 :initform -0.5d0 :documentation "Initial orbital energy"))
   (:documentation "Solar sail orbit problem"))
 
 ;; Propagate a trajectory
@@ -165,7 +171,7 @@ Includes:
    :rs (list (list (* 2 pi) (rotor (bve2 :e1e2 1) (/ pi 2)))
 	     (list (* 4 pi) (rotor (bve2 :e1e2 1) (/ pi 2 3))))))
 
-;; Kustaanheimo-Stiefel spinor states
+;; Spinor states
 
 (defclass spinorstate ()
   ((u :initarg :u :documentation "Spinor of position: r = u e1 (revg u)")
@@ -177,6 +183,12 @@ Includes:
   (format stream "#<SPINORSTATE :U ~a :DUDS ~a :TM ~a>" (slot-value x 'u) (slot-value x 'duds) (slot-value x 'tm)))
 
 (defstatearithmetic spinorstate (u duds tm))
+
+(defmethod energy ((x spinorstate))
+  "Keplerian orbit energy"
+  (with-slots (u duds) x
+    (with-slots (mu) *sc*
+      (/ (- (* 2 (norme2 duds)) mu) (norme2 u)))))
 
 (defmethod eom (s (x spinorstate))
   "Spinor equations of motion: 2 dU/ds - E U = f r U, dt/ds = |r|"
@@ -251,108 +263,100 @@ Includes:
    :rs (rotor (bve2 :e1e2 1d0) (* 35.5 (/ pi 180d0)))))
 
 ;; Kustaanheimo-Stiefel Orbit Element equations of motion
-;; WORK IN PROGRESS - PERTURBED CASE DIVERGES FROM CARTESIAN & SPINOR FORMULATIONS
 
 (defclass ksstate ()
-  ((tm :initarg :tm :documentation "Time")
-   (alpha :initarg :alpha :documentation "U_0")
-   (beta :initarg :beta :documentation "dU/ds_0 / omega")
-   (e :initarg :e :documentation "Work done by the perturbing force"))
-  (:documentation "Kustaanheimo-Stiefel Orbit Element State"))
+  ((alpha :initarg :alpha)
+   (beta :initarg :beta)
+   (w :initarg :w)
+   (t0 :initarg :t0)))
 
-(defstatearithmetic ksstate (tm alpha beta e))
+(defstatearithmetic ksstate (alpha beta w t0))
 
-(defmethod print-object ((x ksstate) stream)
-  (with-slots (tm alpha beta e) x
-    (format stream "#<KSSTATE :TM ~a :ALPHA ~a :BETA ~a :E ~a>" tm alpha beta e)))
+#|
+(defmethod energy ((x ksstate))
+  (with-slots (alpha beta w t0) x
+    (with-slots (mu e0) *sc*
+      (+ e0 
+|#
 
 (defmethod eom (s (x ksstate))
-  "Kustaanheimo-Stiefel Keplerian orbit element equations of motion"
-  (with-slots (tm alpha beta e) x
-    (with-slots (basis accfun x0) *sc*
-      (let* ((w (/ (- (slot-value x0 'e) e) 2d0)) ; work done by disturbing force
-	     (omega (sqrt (/ e -2d0))) ; average orbit angular velocity
-	     (u (+ (* alpha (cos (* omega s))) (* beta (sin (* omega s))))) ; spinor of orbit position
-	     (up (* omega (- (* beta (cos (* omega s))) (* alpha (sin (* omega s)))))) ; du/ds
-	     (r (spin (first basis) u)) ; position vector
-	     (rm (norme2 u)) ; position vector magnitude (radius)
-	     (v (* 2 (/ rm) (*g3 up (first basis) (revg u)))) ; velocity vector
-	     (f (funcall accfun s r v)) ; perturbing force
-	     (ff (/ (- (*g3 f r u) (* w u)) 2d0))) ; KS perturbing force
+  "Kustaanheimo-Stiefel orbit element equations of motion from Arakida and Fukushima"
+  (with-slots (alpha beta w t0) x
+    (with-slots (basis mu accfun e0) *sc*
+      (let* ((hk0 (- e0))
+	     (w0 (sqrt (/ hk0 2)))
+	     (hk (+ (* 2 w) hk0))
+	     (omega (sqrt (/ hk 2)))
+	     (u (+ (* alpha (cos (* omega s)))
+		   (* beta (sin (* omega s)))))
+	     (duds (* omega 
+		      (- (* beta (cos (* omega s)))
+			 (* alpha (sin (* omega s))))))
+	     (rv (spin (first basis) u))
+	     (rm (norme rv))
+	     (vv (* 2 (/ rm) (*g3 duds (first basis) (revg u))))
+	     (fv (funcall accfun s rv vv))
+	     (f (- (dual fv)))
+	     (ff (/ (- (* rm f) (* w u)) 2)))
 	(make-instance
 	 'ksstate
-	 :tm rm
-	 :alpha (* -1 ff (/ omega) (sin (* omega s)))
-	 :beta (* ff (/ omega) (cos (* omega s)))
-	 :e (* rm (scalar (*i f v))))))))
+	 :alpha (* -1 ff (/ (sin (* w0 s)) w0))
+	 :beta (* ff (/ (cos (* w0 s)) w0))
+	 :w (*i f duds)
+	 :t0 (* (/ w0) 
+		(*i ff
+		    (+ (* (- (* alpha (sin (* w0 s)))
+			     (* beta (cos (* w0 s)))) s)
+		       (* (/ (* 2 w0))
+			  (+ (* alpha (cos (* w0 s)))
+			     (* beta (sin (* w0 s)))))))))))))
+
+(defmethod to-spinor (s (x ksstate))
+  "Convert KS state to spinor state"
+  (with-slots (alpha beta w t0) x
+    (with-slots (mu e0) *sc*
+      (let* ((hk0 (- e0))
+	     (w0 (sqrt (/ hk0 2)))
+	     (hk (+ (* 2 w) hk0))
+	     (omega (sqrt (/ hk 2))))
+	(make-instance
+	 'spinorstate
+	 :u (+ (* alpha (cos (* omega s)))
+	       (* beta (sin (* omega s))))
+	 :duds (* omega
+		  (- (* beta (cos (* omega s)))
+		     (* alpha (sin (* omega s)))))
+	 :tm (+ t0
+		(* (/ (+ (exptg alpha 2) (exptg beta 2)) 2) s)
+		(* (/ (- (exptg alpha 2) (exptg beta 2)) (* 4 w0)) (sin (* 2 w0 s)))
+		(* -1 (/ (*i alpha beta) (* 2 w0)) (cos (* 2 w0 s)))))))))
 
 (defmethod to-cartesian (s (x ksstate))
-  "Convert Kustaanheimo-Stiefel orbit element state to cartesian"
-  (with-slots (tm alpha beta e) x
-    (with-slots (basis) *sc*
-      (let* ((omega (sqrt (/ e -2d0)))
-	     (u (+ (* alpha (cos (* omega s))) (* beta (sin (* omega s)))))
-	     (up (* omega (- (* beta (cos (* omega s))) (* alpha (sin (* omega s)))))))
-	(values
-	 (make-instance
-	  'cartstate
-	  :r (spin (first basis) u)
-	  :v (* 2d0 (/ (norme2 u)) (*g3 up (first basis) (revg u))))
-	 tm)))))
-
-(defmethod to-ks (tm (x cartstate))
-  "Make KS element state from time & cartesian state"
-  (with-slots (r v) x
-    (with-slots (mu basis) *sc*
-      (let* ((rvbasis (rvbasis r v))
-	     (u (recoverspinor3d (norme r) rvbasis basis))
-	     (up (/ (*g3 v u (first basis)) 2d0))
-	     (e (/ (- (* 2 (norme2 up)) mu) (norme2 u)))
-	     (omega (sqrt (/ e -2d0))))
+  "Convert KS state to cartesian state"
+  (with-slots (alpha beta w t0) x
+    (with-slots (basis mu e0) *sc*
+      (let* ((hk0 (- e0))
+	     (hk (+ (* 2 w) hk0))
+	     (omega (sqrt (/ hk 2)))
+	     (u (+ (* alpha (cos (* omega s)))
+		   (* beta (sin (* omega s)))))
+	     (duds (* omega 
+		      (- (* beta (cos (* omega s)))
+			 (* alpha (sin (* omega s))))))
+	     (rv (spin (first basis) u))
+	     (rm (norme rv))
+	     (vv (* 2 (/ rm) (*g3 duds (first basis) (revg u)))))
 	(make-instance
-	 'ksstate
-	 :tm tm
-	 :e e
-	 :alpha u
-	 :beta (/ up omega))))))
-
-(defparameter *sail-2d-ks-kepler-eg*
-  (make-instance
-   'sail
-   :tf (* 8 pi)
-   :accfun #'(lambda (s r v) (ve2))
-   :lightness 0.0d0
-   :x0 (let ((*sc* *sail-2d-cart-kepler-eg*))
-	 (to-ks 0 (slot-value *sc* 'x0))))
-  "Unperturbed Kepler problem")
-
-(defparameter *sail-2d-ks-normal-eg*
-  (make-instance
-   'sail
-   :tf (* 4 pi)
-   :pointfun #'sailpointingnormal
-   :lightness 0.1d0
-   :x0 (make-instance
-	'ksstate
-	:tm 0d0
-	:alpha (re2 :s 1d0)
-	:beta (re2 :e1e2 -1d0)
-	:e -0.5d0))
-  "Sail pointed normal to sunlight")
-
-(defparameter *sail-2d-ks-fixed-eg*
-  (make-instance
-   'sail
-   :tf (* 4 pi)
-   :lightness 0.1d0
-   :x0 (make-instance
-	'ksstate
-	:tm 0d0
-	:alpha (re2 :s 1d0)
-	:beta (re2 :e1e2 -1d0)
-	:e -0.5d0)
-   :rs (rotor (bve2 :e1e2 1d0) (* 35.5 (/ pi 180d0))))
-  "Sail pointed at fixed orientation to sunlight")
+	 'cartstate
+	 :r rv
+	 :v vv)))))
+#|
+(defmethod to-ks (s (x spinorstate))
+  (with-slots (u duds tm) x
+    (with-slots (mu x0) *sc*
+      (
+|#      
+      
 
 ;; Export functions
 
@@ -378,50 +382,7 @@ Columns:
   (loop for (s x) in traj
      collect (list (slot-value x 'tm) (to-cartesian s x))))
 
-;;; COE equations of motion
-(defclass coestate ()
-  ((a :initarg :a :documentation "Semi-major axis")
-   (e :initarg :e :documentation "Eccentricity")
-   (i :initarg :i :documentation "Inclination")
-   (lan :initarg :lan :documentation "Longitude of ascending node")
-   (lop :initarg :lop :documentation "Longitude of periapsis")
-   (tm :initarg :tm :documentation "Time"))
-  (:documentation "Classical orbital elements with true anomaly as independent variable"))
-
-(defstatearithmetic coestate (a e i lan lop tm))
-
-;; OLDER STUFF: NEED TO REWORK
-
-;; Classical orbit elements
-(defun coe2rv (sma ecc inc raan aop truan mu basis)
-  "Convert cassical orbital elements to position & velocity vectors.
-SMA semi major axis
-ECC eccentricity
-INC inclination
-RAAN right ascension of ascending node
-AOP argument of perigee
-TRUAN true anomaly
-MU gravitational parameter
-BASIS list of 3 orthogonal basis vectors to express position & velocity in"
-  (let* ((r-raan (rotor (*o (first basis) (second basis)) raan))
-	 (basis-raan (mapcar #'(lambda (x) (rotateg x r-raan)) basis))
-	 (r-inc (rotor (*o (second basis-raan) (third basis-raan)) inc))
-	 (basis-inc (mapcar #'(lambda (x) (rotateg x r-inc)) basis-raan))
-	 (r-aop (rotor (*o (first basis-inc) (second basis-inc)) aop))
-	 (basis-aop (mapcar #'(lambda (x) (rotateg x r-aop)) basis-inc))
-	 (r-truan (rotor (*o (first basis-aop) (second basis-aop)) truan))
-	 (basis-truan (mapcar #'(lambda (x) (rotateg x r-truan)) basis-aop))
-	 (p (* sma (- 1 (* ecc ecc))))
-	 (r (/ p (+ 1 (* ecc (cos truan)))))
-	 (ruv (first basis-truan))
-	 (rv (* ruv r))
-	 (angm (sqrt (/ p mu)))
-	 (angmbv (* (*o (first basis-truan) (second basis-truan)) angm))
-	 (eccuv (first basis-aop))
-	 (eccv (* eccuv ecc))
-	 (vv (* (*g (invv angmbv) (+ eccv ruv))
-		  mu)))
-    (values (graden rv 1) (graden vv 1))))
+;;; Classical orbital element equations of motion
 
 ;; position & velocity to classical orbit elements
 (defun energy-rv (r v mu)
@@ -445,8 +406,8 @@ BASIS list of 3 orthogonal basis vectors to express position & velocity in"
 (defun inc-rv (mombv basis)
   "inclination from momentum bivector and basis"
   (acos (scalar (dual (*o (third basis) (unitg mombv))))))
-(defun raan-rv (nodev basis)
-  "right ascension of ascending node from node vector and basis"
+(defun lan-rv (nodev basis)
+  "Longitude of ascending node from node vector and basis"
   (let ((tmp (atan (scalar (*i nodev (second basis)))
 		   (scalar (*i nodev (first basis))))))
     (if (< tmp 0) (+ tmp (* 2 pi)) tmp)))	 
@@ -476,7 +437,34 @@ BASIS list of 3 orthogonal basis vectors to express position & velocity in"
      :sma (sma-rv (norme rv) (norme vv) mu)
      :ecc (norme eccv)
      :inc (inc-rv mombv basis)
-     :raan (raan-rv nodev basis)
+     :lan (raan-rv nodev basis)
      :aop (aop-rv nodev eccv mombv)
      :truan (truan-rv eccv rv mombv))))
 
+(defun coe2rv (sma ecc inc lan aop truan mu basis)
+  "Convert cassical orbital elements to position & velocity vectors.
+SMA semi major axis
+ECC eccentricity
+INC inclination
+LAN longitude of ascending node
+AOP argument of perigee
+TRUAN true anomaly
+MU gravitational parameter
+BASIS list of 3 orthogonal basis vectors to express position & velocity in"
+  (let* ((r-lan (rotor (*o (first basis) (second basis)) lan))
+	 (basis-lan (new-frame r-lan basis))
+	 (r-inc (rotor (- (dual (first basis-lan))) inc))
+	 (basis-inc (new-frame r-inc basis-lan))
+	 (r-aop (rotor (- (dual (third basis-inc))) aop))
+	 (basis-aop (new-frame r-aop basis-inc))
+	 (r-truan (rotor (- (dual (third basis-aop))) truan))
+	 (basis-truan (new-frame r-truan basis-aop))
+	 (p (* sma (- 1 (expt ecc 2))))
+	 (r (/ p (+ 1 (* ecc (cos truan)))))
+	 (rv (* r (first basis-truan)))
+	 (vv (* (sqrt (/ mu p))
+		(- (* (+ ecc (cos truan)) (second basis-aop))
+		   (* (sin truan) (first basis-aop))))))
+    (values rv vv)))
+
+;;; 
