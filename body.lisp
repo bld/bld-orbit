69b845bc0a8a89894f9681b61e0e35b217bc9f6b
Ben Diedrich <bldiedrich@gmail.com>
2015-04-07 20:09:57 -0700

    position-vector function for bodies

--- body.lisp
+++ body.lisp
@@ -7,6 +7,16 @@
    (ephemeris :initarg :ephemeris)
    (mu :initarg :mu)))
 
+(defmethod position-vector (utc (b body) &key (observer (slot-value b 'center)) (ref (slot-value b 'ref)) (abcorr :none))
+  "Position vector of a body given UTC time, body, and optionally:
+:OBSERVER - name of observing body (default: body's CENTER slot)
+:REF - name of reference frame (default: body's REF slot)
+:ABCORR - aberration correction (default: :NONE)"
+  (with-slots (name ephemeris) b
+    (with-kernel ephemeris
+      (let ((pv (spk-pos name (utc-to-epht utc) observer :ref ref :abcorr abcorr)))
+	(make-instance 've3 :e1 (aref pv 0) :e2 (aref pv 1) :e3 (aref pv 2))))))
+
 (defmethod gravity ((r ve3) (b body))
   "Gravitational acceleration given UTC, position vector of s/c relative to body, and body"
   (with-slots (mu) b
