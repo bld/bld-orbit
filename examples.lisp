fb999cebeb80933434c6bdc9addb1b2299aeb43d
Ben Diedrich <bldiedrich@gmail.com>
2015-04-09 12:40:53 -0700

    Moved kernels to problem class. Using ephemeris time instead of UTC. Working on getting cartesian-test-02 to agree with GMAT.

--- examples.lisp
+++ examples.lisp
@@ -1,12 +1,22 @@
 (in-package :bld-orbit)
 
-(defparameter *ephemeris-dir*
+;; Ephemeris kernels (SPK)
+
+(defparameter *spk-dir*
   (asdf:system-relative-pathname :bld-orbit "ephemeris/"))
+(defparameter *spk* "DE421AllPlanets.bsp")
+(defparameter *spk-path*
+  (princ-to-string (merge-pathnames-as-file *spk-dir* *spk*)))
 
-(defparameter *planetary-ephemeris* "DE421AllPlanets.bsp")
+;; Leap second kernels (LSK)
 
-(defparameter *planetary-ephemeris-path*
-  (princ-to-string (merge-pathnames-as-file *ephemeris-dir* *planetary-ephemeris*)))
+(defparameter *lsk-dir*
+  (asdf:system-relative-pathname :bld-orbit "ephemeris-time/"))
+(defparameter *lsk* "naif0010.tls")
+(defparameter *lsk-path*
+  (princ-to-string (merge-pathnames-as-file *lsk-dir* *lsk*)))
+
+;; Bodies
 
 (defparameter *ssb*
   (make-instance
@@ -14,7 +24,6 @@
    :name :ssb
    :center nil
    :ref :eclipj2000
-   :ephemeris *planetary-ephemeris-path*
    :mu nil))
 
 (defparameter *sun*
@@ -23,7 +32,6 @@
    :name :sun
    :center :ssb
    :ref :eclipj2000
-   :ephemeris *planetary-ephemeris-path*
    :mu 132712440017.99d0))
 
 (defparameter *earth*
@@ -32,7 +40,6 @@
    :name :earth
    :center :ssb
    :ref :eclipj2000
-   :ephemeris *planetary-ephemeris-path*
    :mu 398600.4415d0))
 
 ;;; Test 01
@@ -42,20 +49,26 @@
   (make-instance
    'cartesian-problem
    :central-body *sun*
+   :nbodies nil
+   :spk (list *spk-path*)
+   :lsk *lsk-path*
    :ref :eclipj2000
-   :utc0 *utc-j2000*
-   :utcf (+ *utc-j2000* (convert-unit '(365.25 day) 'second))
-   :eom #'eom-kepler
-   :x0 (to-cartesian *utc-j2000* *earth* :observer :sun :ref :eclipj2000)
-   :hmin (/ 27000 2)
+   :et0 0d0
+   :etf (convert-unit '(365.25 days) 'sec)
+   :eom #'eom-nbody
+   :x0 (with-kernels (*spk-path* *lsk-path*)
+	 (to-cartesian 0d0 *earth* :observer :sun :ref :eclipj2000))
+   :hmin 1d0
    :hmax 27000d0
    :tol 1d-11))
 
+#|
 (defun spinor-test-01 ()
   (to-spinor-problem (cartesian-test-01)))
 
 (defun ks-test-01 ()
   (to-ks-problem (spinor-test-01)))
+|#
 
 ;;; Test 02
 ;;; Spacecraft with initial condition at Sun-Earth L1 with solar and Earth gravity
@@ -65,18 +78,16 @@
    'cartesian-problem
    :central-body *sun*
    :nbodies (list *earth*)
+   :spk (list *spk-path*)
+   :lsk *lsk-path*
    :ref :eclipj2000
-   :utc0 *utc-j2000*
-   :utcf (+ *utc-j2000* (convert-unit '(365.25 days) 'sec))
+   :et0 0d0
+   :etf (convert-unit '(365.25 days) 'sec)
    :eom #'eom-nbody
    :x0 (make-instance
 	'cartesian-state
-	:r (ve3 :e1 -26234828.18431398d0
-		:e2 143254606.368162d0
-		:e3 -606.1106522902846d0)
-	:v (ve3 :e1 -29.49719927764286
-		:e2 -5.414763974791472
-		:e3 0.0001800982443111998))
-   :hmin 1d0
+	:r (ve3 :e1 -26234827.5935499d0 :e2 143254606.4839523d0 :e3 -605.1121386736631d0)
+        :v (ve3 :e1 -29.49719930077679d0 :e2 -5.414763857333822d0 :e3 0.000179932481943812d0))
+   :hmin 1d2
    :hmax 27000d0
-   :tol 1d-13))
+   :tol 1d-12))
