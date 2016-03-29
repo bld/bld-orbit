377087b186188bf36ed016d8dd90958e8741775d
Ben Diedrich <bldiedrich@gmail.com>
2016-03-29 10:20:14 -0500

    Force calculation working. Moment in progress.

--- gsm.lisp
+++ gsm.lisp
@@ -8,17 +8,8 @@
    (bf :initarg :bf)
    (ef :initarg :ef)
    (bb :initarg :bb)
-   (eb :initarg :eb)))
-
-(defparameter *material*
-  (make-instance
-   'material
-   :r 0.91d0
-   :s 0.94d0
-   :bf 0.79d0
-   :ef 0.025d0
-   :bb 0.67d0
-   :eb 0.27d0))
+   (eb :initarg :eb))
+  (:documentation "Sail material optical properties"))
 
 (defmethod a1 ((m material))
   (with-slots (r s) m
@@ -40,7 +31,9 @@
   ((m :initarg :m)
    a1 a2 a3
    (j1 :initarg :j1)
+   j1_v
    (j2 :initarg :j2)
+   j2_v
    (j3 :initarg :j3)
    j3_m
    (l :initarg :l)
@@ -49,29 +42,41 @@
    k3_m)
   (:documentation "Coefficients for use in the GSM force and moment calculations"))
 
-(defparameter *j3*
-  #3a(
-      (
-       (-4.049695115508793e-06     1.577494758486461e-05     6.139288348204577e-04)
-       (1.577494758486461e-05    -2.743513355019449e-06    -5.426699035474121e-05)
-       (6.139288348204577e-04    -5.426699035474120e-05    -5.620418135529490e-03))
-      (
-       (1.577494758486461e-05    -2.743513355019449e-06    -5.426699035474121e-05)
-       (-2.743513355019448e-06     1.075726674329548e-05     6.106349796879265e-04)
-       (-5.426699035474121e-05     6.106349796879265e-04    -2.398752979682822e-02))
-      (
-       (6.139288348204577e-04    -5.426699035474120e-05    -5.620418135529490e-03)
-       (-5.426699035474121e-05     6.106349796879265e-04    -2.398752979682822e-02)
-       (-5.620418135529490e-03    -2.398752979682822e-02     8.583040955919375e+01))))
+
+(defmethod linear-function-2 (m2 (v ve3))
+  "Linear function of a list of VE3 vectors and a VE3"
+  (ve3 :e1 (scalar (*i (first m2) v))
+       :e2 (scalar (*i (second m2) v))
+       :e3 (scalar (*i (third m2) v))))
+
+(defmethod linear-function-3 (m3 (v ve3))
+  "Linear function of a list of lists of VE3 vectors with a vector"
+  (let ((m2 (loop for m2i in m3
+	       collect (linear-function-2 m2i v))))
+    (linear-function-2 m2 v)))
 
 (defmethod initialize-instance :after ((c gsm-coef) &key)
-  (with-slots (m a1 a2 a3 j3 j3_m k3 k3_m) c
+  (with-slots (m a1 a2 a3) c
     (setf
-     ;; Reshape j3 and k3 tensors to 2D
-     j3_m (array-operations:reshape j3 '(9 3))
-     k3_m (array-operations:reshape k3 '(9 3))
      ;; Calculate derived material properties
      a1 (a1 m)
      a2 (a2 m)
      a3 (a3 m))))
 
+(defmethod skew ((vskew ve3))
+  "Form a skew list of VE3 vectors of given VE3 vector"
+  (list
+   (ve3 :e1 0d0 :e2 (gref vskew :e3) :e3 (- (gref vskew :e2)))
+   (ve3 :e1 (- (gref vskew :e3)) :e2 0d0 :e3 (gref vskew :e1))
+   (ve3 :e1 (gref vskew :e2) :e2 (- (gref vskew :e1)) :e3 0d0)))
+
+(defmethod gsm-force-moment ((p number) (solarvb ve3) (c gsm-coef))
+  (let ((sunvb (- solarvb)))
+    (with-slots (a1 a2 a3 j1 j2 j3 l k2 k3) c
+      (let ((f (* p (- (* a2 (linear-function-2 j2 sunvb))
+		       (* a1 (linear-function-3 j3 sunvb))
+		       (* a3 (scalar (*i j1 sunvb)) sunvb))))
+	    #+null(m (* p (- (* a2 (linear-function-2 k2 sunvb))
+		       (* a1 (linear-function-3 k3 sunvb))
+		       (* a3 (linear-function-2 (skew sunvb) (linear-function-2 l sunvb)))))))
+	(values f)))))
